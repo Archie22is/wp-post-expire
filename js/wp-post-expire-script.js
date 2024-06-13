0577f971018d44cc2d187457decbc026725137e4
Archie Makuwa <archie.makuwa@brafton.com>
2024-06-13 23:21:19 +0200

    Consolidated enqueue scripts by combining everything into one single function. Streamlined metabox setup by combining related actions into single functioned. Added commend to improve redability and fixed the timezone settings and sinizated the input. Also inmrpved the jquery/js fuction

--- js/wp-post-expire-script.js
+++ js/wp-post-expire-script.js
@@ -1,15 +1,37 @@
-jQuery(document).ready(function($){
-  $('.datetimepicker').datetimepicker({
-   alwaysSetTime: false,
-   dateFormat: 'd MM yy',
-   timeFormat: 'HH:mm',
-  });
-  $('.datepicker').datepicker({
-    dateFormat: 'd MM yy',
-  });
-  $('.timepicker').timepicker({
-    alwaysSetTime: false,
-    timeFormat: 'HH:mm',
-  });
+/**
+ * WP Post Expire JS
+ * @author Archie M
+ * 
+ * Version: 1.0.1
+ * 
+ */
+
+jQuery(document).ready(function($) {
+  
+  'use strict';
+
+  var dateFormat = 'd MM yy';
+  var timeFormat = 'HH:mm';
+
+  if ($('.datetimepicker').length) {
+      $('.datetimepicker').datetimepicker({
+          alwaysSetTime: false,
+          dateFormat: dateFormat,
+          timeFormat: timeFormat
+      });
+  }
+
+  if ($('.datepicker').length) {
+      $('.datepicker').datepicker({
+          dateFormat: dateFormat
+      });
+  }
+
+  if ($('.timepicker').length) {
+      $('.timepicker').timepicker({
+          alwaysSetTime: false,
+          timeFormat: timeFormat
+      });
+  }
 
 });
